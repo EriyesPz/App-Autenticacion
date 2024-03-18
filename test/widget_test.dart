88eaf15be8a9b32d18eb474c15a295c6029c7f3c
= <pazjosue334@gmail.com>
2024-03-17 23:27:14 -0600

    Version Final

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,26 +6,22 @@ import 'package:app_autenticacion/login_pantalla.dart'; // Importa el archivo in
 
 void main() {
   testWidgets('Inicio de sesión UI test', (WidgetTester tester) async {
-    // Construir el widget de inicio de sesión
+    
     await tester.pumpWidget(MaterialApp(
       home: InicioSesion(),
     ));
 
-    // Encuentra los widgets en la pantalla
     final correoField = find.byType(TextField).first;
     final contrasenaField = find.byType(TextField).last;
     final iniciarSesionButton = find.text('Iniciar Sesión');
     final registrarseButton = find.text('Registrarse');
 
-    // Ingresa texto en los campos de texto
     await tester.enterText(correoField, 'correo@example.com');
     await tester.enterText(contrasenaField, 'contrasena123');
 
-    // Toca el botón de iniciar sesión y espera a que se complete la animación
     await tester.tap(iniciarSesionButton);
     await tester.pump();
 
-    // Verifica que se haya navegado a la pantalla de inicio
     expect(find.text('Pantalla de inicio'), findsOneWidget);
   });
 }
